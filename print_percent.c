4211313f8d8ff97299cc143190c2eddc439b4ebd
OctavioNovelo <octavionovelo22@gmail.com>
2024-04-02 18:32:29 +0000

    Correction_1

--- print_percent.c
+++ print_percent.c
@@ -1,17 +1,23 @@
 #include "main.h"
+
 /**
+ * print_percent - Print a percent sign if the input string is a percent sign.
+ * @str: A string containing the character to print.
  *
+ * Return: 1 if a percent sign is printed, otherwise 0.
  */
 int print_percent(char *str)
 {
-  if (str == NULL)
-     {
-       return NULL;
-     }
-  if (*str == '%')
+    if (str == NULL)
     {
-      _putchar('%');
-      return 0;
+        return 0;
     }
-   return 0;
+
+    if (*str == '%')
+    {
+        _putchar('%');
+        return 1;
+    }
+
+    return 0;
 }
