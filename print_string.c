3f4f84e281d44d32821822b246be7de70a8efb7a
Dierak <diego.b.c.rodriguez@gmail.com>
2024-04-19 23:58:12 +0000

    Applying style

--- print_string.c
+++ print_string.c
@@ -9,17 +9,18 @@
 
 int print_string(va_list args)
 {
-  char *str = va_arg(args, char *);
-  int printed_chars;
-  printed_chars = 0;
-    if (str == NULL)
+	char *str = va_arg(args, char *);
+	int printed_chars;
+	printed_chars = 0;
+	
+	if (str == NULL)
         str = "(null)";
+	
+	while (*str)
+	{
+        	printed_chars += _putchar(*str);
+        	str++;
+    	}
 
-    while (*str)
-    {
-        printed_chars += _putchar(*str);
-        str++;
-    }
-
-    return printed_chars;
+    	return printed_chars;
 }
