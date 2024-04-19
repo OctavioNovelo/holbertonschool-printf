c586506c5d2de066635a481df1cc6a75f14c4e92
Dierak <diego.b.c.rodriguez@gmail.com>
2024-04-19 23:51:18 +0000

    Added documentation

--- print_char.c
+++ print_char.c
@@ -1,7 +1,12 @@
 #include "main.h"
+
 /**
+ * print_char - Prints a character
+ * @args: Arguments
  *
+ * Return: A character
  */
+
 int print_char(va_list args)
 {
    int character = va_arg(args, int);
