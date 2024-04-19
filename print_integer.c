95a4ed271e036b8ceef0b32d7192f85488c150cc
Dierak <diego.b.c.rodriguez@gmail.com>
2024-04-19 23:33:33 +0000

    print_integer

--- print_integer.c
+++ print_integer.c
@@ -6,8 +6,9 @@
  *
  * Return: The number of characters printed.
  */
+
 int print_integer(va_list args)
-{
+/**{
     int num;
     int count = 0;
 
@@ -35,4 +36,35 @@ int print_integer(va_list args)
     count++;
 
     return count;
+}
+*/
+{
+	int value;
+	unsigned int abs, a, len;
+	unsigned int countn = 1;
+
+	len = 0;
+
+	value = va_arg(args, int);
+
+	if (value < 0)
+	{
+		len = len + _putchar('-');
+		abs = value * -1;
+	}
+	else
+		abs = value;
+
+	a = abs;
+	while (a > 9)
+	{
+		a = a / 10;
+		countn = countn * 10;
+	}
+	while (countn >= 1)
+	{
+		len = len + _putchar(((abs / countn) % 10) + '0');
+		countn = countn / 10;
+	}
+	return (len);
 }
