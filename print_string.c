98481176e3f8c4eb619203ee3d751f37aeaf70eb
Dierak <diego.b.c.rodriguez@gmail.com>
2024-04-20 00:14:46 +0000

    Applying bettu style

--- print_string.c
+++ print_string.c
@@ -11,16 +11,17 @@ int print_string(va_list args)
 {
 	char *str = va_arg(args, char *);
 	int printed_chars;
+
 	printed_chars = 0;
-	
+
 	if (str == NULL)
-        str = "(null)";
-	
+	{
+	str = "(null)";
+	}
 	while (*str)
 	{
-        printed_chars += _putchar(*str);
-        str++;
-    	}
-
-    	return printed_chars;
+	printed_chars += _putchar(*str);
+	str++;
+	}
+	return (printed_chars);
 }
